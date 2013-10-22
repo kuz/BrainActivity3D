9fbadc71286b43a37351937ea1415b4985895be8
Ilja Kuzovkin <ilja.kuzovkin@gmail.com>
2013-10-23 00:33:07 +0300

    Simplified brain and kinda light

--- brainactivity.py
+++ brainactivity.py
@@ -28,17 +28,31 @@ def init():
 
     # Enable Z-Buffer
     glEnable(GL_DEPTH_TEST)
-    glEnable(36281)
+    #glEnable(36281)
+
+    # Re-compute normals
+    glEnable(GL_AUTO_NORMAL)
+
     # Perpective
     glMatrixMode(GL_PROJECTION)
     gluPerspective(45, 4/3, 0.5, 40)
-    #glOrtho(-10, 10, -10, 10, -10, 10)
+
     glMatrixMode(GL_MODELVIEW)
 
     # Enable basic lighting
-    glEnable(GL_COLOR_MATERIAL)
+    #glEnable(GL_COLOR_MATERIAL)
     glEnable(GL_LIGHTING)
+
+    # Put all defaults to zero
+    glEnable(GL_LIGHT0)
+    #glLightfv(GL_LIGHT0, GL_AMBIENT, [0, 0, 0, 0])
+    #glLightfv(GL_LIGHT0, GL_SPECULAR, [0, 0, 0, 0])
+    #glLightfv(GL_LIGHT0, GL_DIFFUSE, [0, 0, 0, 0])
+    #glLightModelfv(GL_LIGHT_MODEL_AMBIENT, [0, 0, 0, 0])
+
+    # Backface culling
     glEnable(GL_CULL_FACE)
+    
     # Initialize model
     init_model()
 
@@ -63,45 +77,36 @@ def display():
     Main drawing function
     """
     global brain
-    #Light source 0
-    glLightfv(GL_LIGHT0, GL_POSITION, [0,1,0,0])
-    glLightfv(GL_LIGHT0, GL_SPECULAR, [1,0,0,1])
-    glEnable(GL_LIGHT0)
+
     # Clear screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, [0.5,0.5,0.5,1])
-    glMaterialfv(GL_FRONT, GL_SPECULAR, [0.2,.2,.2,1])
+
+    #Light source 0
+    glLightfv(GL_LIGHT0, GL_POSITION, [0, 1, 0, 0])
+    glLightfv(GL_LIGHT0, GL_SPECULAR, [0, 0, 0, 1])
+    
+    # Material    
+    glMaterialfv(GL_FRONT, GL_AMBIENT, [0.5, 0.5, 0.5, 1])
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, [0.5, 0.5, 0.5, 1])
+    glMaterialfv(GL_FRONT, GL_SPECULAR, [0.8, 0.8, 0.8, 1])
+    
     glLoadIdentity()
     gluLookAt(0, 20, 20, 0, 0, 0, 0, 0, 1)
     
-    #glOrtho(-5.0, 5.0, -5.0, 5.0, -5.0, 5.0)
-
-    #glLightfv(GL_LIGHT0, GL_POSITION, [-5, 5, 0.5, 1])
-
     glScalef(0.1, 0.1, 0.1)
 
-    #glPushMatrix()
-    #glTranslatef(0, 0, 0)
+    glPushMatrix()
+    glTranslatef(0, 0, 0)
     #glRotatef(90,1,0,0)
     #glutSolidTeapot(3)
-    #glPopMatrix()
+    #glutSolidTorus(3, 5, 30, 30)
+    glPopMatrix()
     
-    glRotatef(glutGet(GLUT_ELAPSED_TIME)*0.03, -1, 0, 0)
-    
-    #glPushMatrix()
-    #glTranslatef(0, 0.5, 0)
-    #glRotatef(90, 1, 0, 0)
-    #glColor(1, 0, 0);
-    #glutSolidCylinder(0.3, 1, 10, 10)
-    #glPopMatrix()
-
-    #glColor(0, 1, 0);
-    #glRectf(-1, 1, 1, -1)
-
     # Draw brain
-    #glTranslatef(0, 0.5, 0)
-    
+    glPushMatrix()
+    glRotatef(glutGet(GLUT_ELAPSED_TIME)*0.03, -1, 0, 0)
     glCallList(brain.gl_list)
+    glPopMatrix()
 
     # Switch buffers
     glutSwapBuffers()
@@ -129,9 +134,8 @@ def init_model():
     Load model from Wavefront .obj file
     """
     global brain
-
-    brain = objloader.OBJ('brain.obj', 'model', swapyz=False)
-    #brain = objloader.OBJ('al.obj', 'model', swapyz=False)
+    brain = objloader.OBJ('brain_simplified.obj', 'model', swapyz=False)
+    #brain = objloader.OBJ('brain.obj', 'model', swapyz=False)
 
 def main():
     """
